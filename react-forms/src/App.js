dc873375ba06ef83ad38847aea0612711b7a982a
Natasha <natasha.marie33@gmail.com>
2025-04-21 17:48:52 +0900

    finished forms

--- react-forms/src/App.js
+++ react-forms/src/App.js
@@ -8,9 +8,15 @@ function App() {
       email:"", 
       dob:"",
       gender: "",
-      prompt1: "",
-      answer1: "",
   })
+
+
+  {/*we need to break out the prompt and answer from the user info and move it to a new state. so in this new state we want to set the default to not just be an obj but an array of objs, because we want to keep pushing the prompts into the collection of prompts as the user keeps adding them. so those need to be added to our array.*/ }
+  const [prompts, setPrompts] = useState([{
+    prompt: "",
+    answer: "",
+    timestamp: new Date().getTime()
+  }])
 
 {/*the ...userInfo's "..." below is used as a rest syntax here to collect all remaining elements into an array in the format of name and value. "..." is also a spread operator which unpacks collected elements like an array into single elements. kinda like a suitcase, you're either packing things in or out*/}
   const handleInput = e => {
@@ -19,6 +25,27 @@ function App() {
       ...userInfo,
       [name]: value
     })
+  }
+
+  const handlePrompt = (e, i) => {
+    const {name, value} = e.target;
+    let newPrompts = [...prompts]; {/*current collection of prompts*/}
+    newPrompts [i] [name] = value;
+    setPrompts(newPrompts);
+  }
+
+  const handleAddPrompt = () => {
+    setPrompts([...prompts, {
+      prompt: "",
+      answer: "",
+      timestamp: new Date().getTime()
+    }])
+  }
+
+  const handleDelete = (i) => {
+    let deletePrompts = [...prompts];
+    deletePrompts.splice(i, 1);
+    setPrompts(deletePrompts);
   }
 
 
@@ -81,24 +108,33 @@ function App() {
         </fieldset>
         <fieldset className="fieldsetContainer">
           <legend>Prompts</legend>
-          <div>
+          {prompts.map((prompt, i) => (
+          <div key={prompt.timestamp}>
             <label>Select a Prompt</label>
-            <select>
+            <div classname="deleteButton">
+            <select id="prompt" name="prompt" onChange={e => handlePrompt(e, i)}>
               <option value="My favorite anime is...">My favorite anime is...</option>
               <option value="Guinea pigs are awesome because...">Guinea pigs are awesome because...</option>
               <option value="On my days off I like to...">On my days off I like to...</option>
               <option value="My absolute recommendation is...">My absolute recommendation is...</option>
               <option value="Something unique about me is...">Something unique about me is...</option>
             </select>
+             {/*since we want to feed through the index number, it'll be easier if the delete is inside a map method*/ }
+            <button type="button" onClick={() => handleDelete(i)}>Delete</button>
+            </div>
             <textarea
               id="answer1"
               name="answer1"
               rows={5}
               placeholder="Be honest"
-              onChange={handleInput}/>
+              onChange={e => handlePrompt(e, i)}/>
           </div>
-          <div>
-            <button>Add Prompt</button>
+          ))}
+          <div className="addPrompt">
+            <button
+            type="button"
+            onClick={handleAddPrompt}>
+              Add Prompt</button>
           </div>
         </fieldset>
 
